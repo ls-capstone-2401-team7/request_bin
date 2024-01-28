ca4c1146f184575db41059920da0ce5ea42fb573
AlessioNovi <alessionovi@yahoo.com>
2024-01-28 01:07:28 +0100

    test-branch-works

--- backend/setup_postgres.sh
+++ backend/setup_postgres.sh
@@ -4,8 +4,8 @@
 sudo -u postgres bash -c "psql -c \"CREATE USER request_bin WITH SUPERUSER PASSWORD 'password';\""
 
 # create fresh database
-sudo -u postgres psql dropdb request_bin
-sudo -u postgres psql createdb request_bin
+sudo -u postgres dropdb --if-exists request_bin
+sudo -u postgres createdb request_bin
 
 # create schema
 sudo -u postgres psql -d request_bin < schema.sql
