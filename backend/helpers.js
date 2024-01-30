f11aac83ad7ef1993131558b6db75b30ed9bc14c
Alex Bair <alexbair143@gmail.com>
2024-01-29 20:26:20 -0500

    Created helper function for generating UUIDs.

--- backend/helpers.js
+++ backend/helpers.js
@@ -1,3 +1,26 @@
+/* eslint-disable no-param-reassign */
+const { v4: uuidv4 } = require('uuid');
+const psqlDb = require('./db/postgres_utils');
+
+async function isValidUuid(inputUuid) {
+  const result = await psqlDb.getBinId(inputUuid);
+  return !result;
+}
+
+async function generateUuid() { // check & confirm works later
+  let newUuid = uuidv4();
+
+  while (true) {
+    let valid = await isValidUuid(newUuid);
+    if (valid) { 
+      break;
+    }
+    newUuid = uuidv4();
+  }
+
+  return newUuid;
+}
+
 function convertDbTimetoDateObj(databaseTime) {
   databaseTime[10] = 'T';
   databaseTime = `${databaseTime.substring(0, 23)}Z`;
@@ -6,4 +29,9 @@ function convertDbTimetoDateObj(databaseTime) {
 
 module.exports = {
   convertDbTimetoDateObj,
+  generateUuid,
 };
+
+(async function () {
+  console.log(await generateUuid());
+})();
