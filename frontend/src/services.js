b655cadb98b4c455d5a5bb54dcc883fc41d79947
Alex Bair <alexbair143@gmail.com>
2024-02-01 22:08:22 -0500

    Changed URL in services.js to the actual domain name, NOT localhost.

--- frontend/src/services.js
+++ frontend/src/services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // create new bin
-const URL = "http://localhost:3000/api/bins";
+const URL = "https://alex-bair.com/api/bins"; // make sure to update this with your domain! CANNOT USE LOCALHOST HERE!
 
 async function createBin() {
   const result = await axios.post(URL);
