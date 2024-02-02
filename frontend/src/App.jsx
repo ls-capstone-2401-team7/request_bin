9c879a86eb7e5211c05d0011827c4e8b22237e62
Alex Bair <alexbair143@gmail.com>
2024-02-01 21:11:46 -0500

    Removed debugging code.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import { io } from 'socket.io-client';
+// import { io } from 'socket.io-client';
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Bin from "./components/Bin";
-import { useEffect  } from "react";
+// import { useEffect  } from "react";
 
 
 import "./App.css";
-const socket = io("http://localhost:3000");
+// const socket = io("http://localhost:3000");
 
 function App() {
   // const [count, setCount] = useState(0)
 
   
 
-  useEffect(() => {
-    socket.emit("frontend", "helloefromfrontendzooor");
+  // useEffect(() => {
+  //   socket.emit("frontend", "helloefromfrontendzooor");
 
-    const testRequest = async () => fetch("http://localhost:3000");
-    testRequest();
-    socket.on("backend", (message) => console.log(message));
-  }, []);
+  //   const testRequest = async () => fetch("http://localhost:3000");
+  //   testRequest();
+  //   socket.on("backend", (message) => console.log(message));
+  // }, []);
   return (
     <>
       <Routes>
